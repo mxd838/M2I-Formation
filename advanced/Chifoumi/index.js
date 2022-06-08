e9bd25cb3f2ff67bea95b272ca4e3152a8e050a8
mxd838 <mxduran@protonmail.com>
2022-06-08 21:59:09 +0200

    add finished javascript

--- advanced/Chifoumi/index.js
+++ advanced/Chifoumi/index.js
@@ -1,14 +1,16 @@
 // Get elements
 const computerChoiceDisplay = document.querySelector('.computerChoiceImg')
-const choicePaper = document.querySelector('.paper')
-const choiceRock = document.querySelector('.rock')
-const choiceScissors = document.querySelector('.scissors')
+
+const userChoiceSelection = document.querySelector('.userChoices')
 const computerScoreDisplay = document.querySelector('.computerScore')
 const userScoreDisplay = document.querySelector('.userScore')
 const resultSentence = document.querySelector('.resultSentence')
 const resetBtn = document.querySelector('.reset')
 const displayNumberOfRounds = document.querySelector('.numberOfRounds')
 const confirmButton = document.querySelector('.confirm')
+const lessButton = document.querySelector('.less')
+const moreButton = document.querySelector('.more')
+const roundsSelection = document.querySelector('.selection')
 
 // Variables
 let userScore = 0
@@ -17,12 +19,61 @@ let roundLimit = 3
 let numberOfRounds = 0
 
 
-// for the reset
-// --- put the choice of rounds in a div setNumberOfRounds
-// --- set the roundlimit to the value retrieved
-// --- once the value chosen and confirmed (via button) 
-// --- turn the div setNumberOfRounds to display none
+// Creation and insertion of the images
+const images = [
+    {
+        class: 'paper',
+        src: "./img/paper.png",
+        alt: 'paper'
+    },
+    {
+        class: 'rock',
+        src: "./img/rock.png",
+        alt: 'rock'
+    },
+    {
+        class: 'scissors',
+        src: "./img/scissors.png",
+        alt: 'scissors'
+    }
+]
+
+for (let image of images) {
+    // Creation of the elements
+    const imgContainer = document.createElement('div')
+    const imgWeapon = document.createElement('img')
+    // Images attributes
+    imgWeapon.className = image.class
+    imgWeapon.src = image.src
+    imgWeapon.alt = image.alt
+    // Insertion
+    imgContainer.appendChild(imgWeapon)
+    userChoiceSelection.appendChild(imgContainer)
+}
+// Selection of the elements created
+const choicePaper = document.querySelector('.paper')
+const choiceRock = document.querySelector('.rock')
+const choiceScissors = document.querySelector('.scissors')
+
 // Functions
+const hideNumberOfRoundsChoice = () => {
+    console.log(roundsSelection.style)
+    roundsSelection.style.display = 'none'
+}
+
+const lessRounds = () => {
+    if (roundLimit >= 3){
+        roundLimit -= 2
+        displayNumberOfRounds.innerText = roundLimit
+    }
+}
+
+const moreRounds = () => {
+    if (roundLimit <= 11){
+        roundLimit += 2
+        displayNumberOfRounds.innerText = roundLimit
+    }
+}
 const setComputerChoice = () => {
     const computerChoices = ['rock', 'paper', 'scissors']
     const computerChoice = computerChoices[Math.floor(Math.random() * 3)]
@@ -36,8 +87,12 @@ const setUserChoice = (e) => {
 
 const resetGame = () => {
     userScore = 0
+    userScoreDisplay.innerText = 0
     computerScore = 0
+    computerScoreDisplay.innerText = 0
     numberOfRounds = 0
+    roundsSelection.style.display = 'block'
+    resultSentence.innerText = 'Jouons encore !'
 }
 
 const checkEndgame = () => {
@@ -48,38 +103,45 @@ const checkEndgame = () => {
         } else {
             resultSentence.innerText = "L'ordinateur a gagné la partie"
         }
+        return true
     }
+    return false
 }
 
 
 const playGame = (e) => {
-    const userChoice = setUserChoice(e)
-    const computerChoice = setComputerChoice()
-    
-    if ((userChoice === 'rock' && computerChoice === 'scissors') 
-    || (userChoice === 'paper' && computerChoice === 'rock')
-    || (userChoice === 'scissors' && computerChoice === 'paper') )
-    {
-        userScore++
-        userScoreDisplay.innerText = userScore
-        resultSentence.innerText = 'Le joueur a gagné le round'
-        numberOfRounds++
-    } else if ( userChoice === computerChoice){
-        // console.log('Draw')
-        resultSentence.innerText = 'Match nul'
-    } else {
-        computerScore++
-        computerScoreDisplay.innerText = computerScore
-        resultSentence.innerText = "L'ordinateur a gagné le round"
-        numberOfRounds++
-    }
-    
-    
-    checkEndgame()
 
+    if (checkEndgame() === false){
+
+        hideNumberOfRoundsChoice()
+
+        const userChoice = setUserChoice(e)
+        const computerChoice = setComputerChoice()
+        
+        if ((userChoice === 'rock' && computerChoice === 'scissors') 
+        || (userChoice === 'paper' && computerChoice === 'rock')
+        || (userChoice === 'scissors' && computerChoice === 'paper') )
+        {
+            userScore++
+            userScoreDisplay.innerText = userScore
+            resultSentence.innerText = 'Le joueur a gagné le round'
+            numberOfRounds++
+        } else if ( userChoice === computerChoice){
+            resultSentence.innerText = 'Match nul'
+        } else {
+            computerScore++
+            computerScoreDisplay.innerText = computerScore
+            resultSentence.innerText = "L'ordinateur a gagné le round"
+            numberOfRounds++
+        }
+    }
+    checkEndgame()
 }
 // Event listeners
 choicePaper.addEventListener('click', playGame)
 choiceRock.addEventListener('click', playGame)
 choiceScissors.addEventListener('click', playGame)
 resetBtn.addEventListener('click', resetGame)
+confirmButton.addEventListener('click', hideNumberOfRoundsChoice)
+lessButton.addEventListener('click', lessRounds)
+moreButton.addEventListener('click', moreRounds)
