094d535f79a5581bb58f77c6528a76accc377f9f
mxd838 <mxduran@protonmail.com>
2022-06-14 14:46:47 +0200

    add tre and some memory logic

---
+++ TRE/notes.md
@@ -0,0 +1,60 @@
+postuler dès aujourd'hui pour disponibilité octobre
+loisirs : 
+veille technologique
+
+https://www.youtube.com/watch?v=rZr23bMwHsU
+
+
+développeur JavaScript
+
+
+https://medium.com/quick-code/7-javascript-concepts-you-should-be-familiar-with-as-a-developer-e8d0369cc81b
+
+
+https://www.infoq.com/articles/staff-plus-open-source-engagement/?utm_campaign=infoq_content&utm_source=infoq&utm_medium=feed&utm_term=global
+
+ramener bloc notes (entretien)
+
+
+welovedevs
+indeed
+welcome to the jungle
+
+
+jira
+trello
+monday.com
+basecamp
+slack
+wrike
+asana
+msproject
+openproj
+projectlibre
+
+
+mind mapping
+x-mind
+
+
+outil sur pole emploi > la bonne boite pour voir les futurs besoins des entreprises inscrites
+
+
+Structure de la lettre de motivation
+1- Accroche
+2- Infos sur le poste, l'organisation
+3- Moi, candidat
+4- Toi et moi (collaboration entre orga et moi)
+5- Disponibilité
+6- Politesse
+
+Rappel : lettre de motivation
+* Accroche lecteur/ recruteur
+* Communication infos autres que dans CV
+* Atouts par rapport au poste visé
+* Montrer ou démontrer vos plus values sur le poste
+* Réponses aux besoins (poste) et attentes (recruteurs)
+Objectif : décrocher un entretien
+c'est aussi proposer une collaboration et non pas rechercher un emploi
+c'est offrir des compétences et non pas un demandeur d'emploi
+c'est bien souvent une question de posture
--- advanced/Memory/index.js
+++ advanced/Memory/index.js
@@ -20,6 +20,7 @@
 
 const container = document.querySelector('.container')
 
+let imageNumber = 0
 
 // Create images grid
 for (let i = 0; i < 4; i++){
@@ -30,50 +31,47 @@ for (let i = 0; i < 4; i++){
     for (let j = 0; j < 4; j++) {
         const cardContainer = document.createElement('div')
         cardContainer.classList.add('cardContainer')
-        cardContainer.classList.add(`cell${i}${j}`)
         cardRow.appendChild(cardContainer)
+        const questionMarkImg = document.createElement('img')
+        questionMarkImg.setAttribute('src', './img/questionMark.png')
+        questionMarkImg.classList.add(`cell${imageNumber}`)
+        cardContainer.appendChild(questionMarkImg)
+        imageNumber++
     }
 }
 
 // Get elements generated
-const cardContainers = document.querySelectorAll('.cardContainer')
+const imgCells = document.querySelectorAll('.cell')
 
-const imagesSrc = ['./img/afghan.png','./img/albania.png', './img/algeria.png', './img/andorra.png', './img/angola.png', '.img/antigua-barbada.png', './img/argentina.png', './img/bangladesh.png']
+// console.log(imgCells)
 
 
-const createImagesObjects = () => {
-    const imagesObjectsArray = []
-    for (let imageSrc in imagesSrc){
-        const imgObject = {
-            id: `imageId${imageSrc}`,
-            verso: imagesSrc[imageSrc],
-            returned: false
-        }
-        imagesObjectsArray.push(imgObject)
+
+const randomizeImages = () => {
+    const imagesSrc = ['./img/afghan.png','./img/albania.png', './img/algeria.png', './img/andorra.png', './img/angola.png', '.img/antigua-barbada.png', './img/argentina.png', './img/bangladesh.png']
+    const completeImagesArray = imagesSrc.concat(imagesSrc)
+    for (let i = completeImagesArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = completeImagesArray[i];
+        completeImagesArray[i] = completeImagesArray[j];
+        completeImagesArray[j] = temp;
     }
-    return imagesObjectsArray
+    // insert data-src attribute for each image and use it to toggle src when click
+    return completeImagesArray
 }
 
-
-const createImagesElements = () => {
-    console.log('create img elts')
-    for (let imgData of imagesData){
-        console.log(imgData)
-        const imgToGuess = document.createElement('img')
-        imgToGuess.setAttribute('src', './img/questionMark.png')
-    }
-}
-
-const randomizeImagesOrder = () => {
-    // order of the images in the divs is random
-}
 
 const placeImages = () => {
-    cardContainers.forEach( imageContainer => {
-        console.log(imageContainer)
-    })
+    const randomizedArray = randomizeImages()
+     
 }
 
-// Function calls
-const imagesData = createImagesObjects()
-createImagesElements()
+const toggleImage = (e) => {
+    console.log(e.target.className)
+    // e.target.src === './img/questionMark.png' ? ``
+}
+
+
+imgCells.forEach(imgCell => {
+    imgCell.addEventListener('click', toggleImage)
+})
